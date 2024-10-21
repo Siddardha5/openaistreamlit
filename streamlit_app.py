585020f66661034be584267e8b6a6693fc7e8823
Siddardha Nelapudi <156110982+Siddardha5@users.noreply.github.com>
2024-10-20 23:23:47 -0400

    v3.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from openai import OpenAI
 import os
 
-st.title("My Super Awesome OpenAI API Deployment!")
+st.title("My Super Awesome OpenAI API Deployment!!")
 
 prompt = st.text_input("What is your prompt today?", "Damascus is")
 
